9ed50cbc89daff7280d176367c5c6500886fb9ea
asllan <asllan77@gmail.com>
2017-07-17 17:29:46 -0400

    create PlaySoundsViewController and link it to the stroyboard

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Ukjin Lee on 2017-07-17.
+//  Copyright © 2017 ukjin. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
