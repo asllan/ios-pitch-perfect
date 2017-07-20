ac3fb418178e3ca4538858a758e7aef52a94bd10
asllan <asllan77@gmail.com>
2017-07-20 12:06:23 -0400

    play back audio

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -40,11 +40,26 @@ class PlaySoundsViewController: UIViewController {
         configureUI(.notPlaying)
     }
     
-    @IBAction func playSound(_ sender: Any) {
-        print("play sound")
+    @IBAction func playSound(_ sender: AnyObject) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopSound(_ sender: Any) {
-        print("stop sound")
+        stopAudio()
     }
 }
